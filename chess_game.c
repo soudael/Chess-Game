f8fae3417c9119cba3fa97242d0d52c01c092112
soudael <raitohasshin@gmail.com>
2024-12-15 23:05:01 -0300

    B&W Pawn

--- chess_game.c
+++ chess_game.c
@@ -1,27 +1,34 @@
-// chess_game.c // version 0.1.0-alpha // debugging stage -- 12/15/24
+// chess_game.c // version 0.1.1-alpha // --debugging
 #include <stdio.h>
 #include <string.h>
 
 /*PAWN MOVEMENT FUNCTION*/
-int pawn_Move(char start_pos[3], char end_pos[3])
+int pawn_Move(char start_pos[3], char end_pos[3], int color)
 {
-    if(end_pos[0] != start_pos[0]) return 0;
-    if(end_pos[1] <= start_pos[1]) return 0;
-    if(end_pos[1] > start_pos[1]+2) return 0;
-
-    return 1;
+    if (color){
+        if (end_pos[0]==start_pos[0] && end_pos[1]>start_pos[1] && end_pos[1]<start_pos[1]+3)
+            return 1;
+    }else{
+        if (end_pos[0]==start_pos[0] && end_pos[1]<start_pos[1] && end_pos[1]>start_pos[1]-3)
+            return 1;
+    }
+    return 0;
 }
 
 /*PAWN CAPTURE FUNCTION*/
-int pawn_Capture(char start_pos[3], char end_pos[3])
+int pawn_Capture(char start_pos[3], char end_pos[3], int color)
 {
-    if(end_pos[0] != start_pos[0]+1 && end_pos[0] != start_pos[0]-1) return 0;
-    if(end_pos[1] != start_pos[1]+1) return 0;
-
-    return 1;
+    if (color){
+        if(end_pos[0]==start_pos[0]+1 && end_pos[0]==start_pos[0]-1 && end_pos[1]==start_pos[1]+1)
+            return 1;
+    }else{
+        if(end_pos[0]==start_pos[0]+1 && end_pos[0]==start_pos[0]-1 && end_pos[1]==start_pos[1]-1)
+            return 1;
+    }
+    return 0;
 }
 
-main()
+int main()
 {
     char piece_name[4][8] = {{'r','n','b','q','k','b','n','r'},
                              {'p','p','p','p','p','p','p','p'},
@@ -31,9 +38,11 @@ main()
     int square_status[8][8], board_layout[8][8], piece_type[8][8];
     char start_pos[3], end_pos[3], chessboard[8][8][3];
     int run_WhiteMove, run_BlackMove, run_WhiteCapture, run_BlackCapture;
+    int color;
     int i, j;
 
     /*
+    Along the code, positive(true) values means white pieces, and negative(false) values means black pieces.
     piece_name: determines the name (identifier) of each piece in the game.
     board_layout: assigns the name (identifier) of each piece to its respective positions.
     square_status: assigns a value of 0 (empty) or 1 (occupied) to each square on the board.
@@ -41,6 +50,7 @@ main()
     start_pos & end_pos: variables for reading the move of each player.
     chessboard: contains all constant and specific coordinates of the board.
     run_ColorAction: variables for controlling the execution of specific functions.
+    color: decides between running white or black functions.
     */
 
     for(i=0;i<8;i++)
@@ -122,7 +132,7 @@ main()
     }
 
     printf("\n Make your move:\n ");
-    scanf("%2s>%2s", &start_pos, &end_pos);
+    scanf("%2s>%2s", start_pos, end_pos);
 
     run_WhiteMove=0;
     run_BlackMove=0;
@@ -134,72 +144,73 @@ main()
     for(i=0;i<8;i++)
         for(j=0;j<8;j++)
             if(strcmp(start_pos, chessboard[i][j]) == 0){
-                printf(" >> tile exists.\n\n");
-                if(square_status[i][j] && piece_type[i][j]>0){
-                    run_WhiteMove=1;
-                    run_WhiteCapture=1;
-                }else
-                if(square_status[i][j] && piece_type[i][j]<0){
-                    run_BlackMove=1;
-                    run_BlackCapture=1;
+                printf(" >> start tile exists.\n\n");
+                if(square_status[i][j]){
+                    if (piece_type[i][j]>0){
+                        color=1;
+                    }else
+                    if (piece_type[i][j]<0){
+                        color=0;
+                    }
                 }else
                     printf(" >> tile without present pieces.\n\n");
-            }else
-                ;
+            }
 
     printf(" run_WhiteMove=%d\n", run_WhiteMove);
     printf(" run_BlackMove=%d\n", run_BlackMove);
     printf(" run_WhiteCapture=%d\n", run_WhiteCapture);
-    printf(" run_BlackCapture=%d\n\n", run_BlackCapture);
+    printf(" run_BlackCapture=%d\n", run_BlackCapture);
+    printf("\n color: %d\n\n", color);
 
     for(i=0;i<8;i++)
         for(j=0;j<8;j++)
-            if(strcmp(end_pos, chessboard[i][j]) == 0)
-                if(square_status[i][j] && run_WhiteCapture)
-                    if(piece_type[i][j]<0)
-                        run_WhiteMove=0;
-                    else
-                        run_WhiteCapture=0;
-                else
-                if(square_status[i][j] && run_BlackCapture)
-                    if(piece_type[i][j]>0)
-                        run_BlackMove=0;
-                    else
-                        run_BlackCapture=0;
-                else
-                if(!square_status[i][j] && run_WhiteMove)
-                    run_WhiteCapture=0;
-                else
-                if(!square_status[i][j] && run_BlackMove)
-                    run_BlackCapture=0;
-
+            if(strcmp(end_pos, chessboard[i][j]) == 0){
+                printf(" >> end tile exists.\n\n");
+                if(color){
+                    if(piece_type[i][j]<=0){
+                        if(square_status[i][j])
+                            run_WhiteCapture=1;
+                        else
+                            run_WhiteMove=1;
+                    }
+                }else{
+                    if(piece_type[i][j]>=0){
+                        if(square_status[i][j])
+                            run_BlackCapture=1;
+                        else
+                            run_BlackMove=1;
+                    }
+                }
+            }
 
     printf(" run_WhiteMove=%d\n", run_WhiteMove);
     printf(" run_BlackMove=%d\n", run_BlackMove);
     printf(" run_WhiteCapture=%d\n", run_WhiteCapture);
     printf(" run_BlackCapture=%d\n\n", run_BlackCapture);
 
-    if(run_WhiteCapture)
-        if(pawn_Capture(start_pos, end_pos))
+    if(run_WhiteCapture){
+        if(pawn_Capture(start_pos, end_pos, color))
             printf(" valid\n");
         else
             printf(" invalid\n");
-    else
-    if(run_BlackCapture)
-        if(pawn_Capture(start_pos, end_pos))
+    }else
+    if(run_BlackCapture){
+        if(pawn_Capture(start_pos, end_pos, color))
             printf(" valid\n");
         else
             printf(" invalid\n");
-    else
-    if(run_WhiteMove)
-        if(pawn_Move(start_pos, end_pos))
+    }else
+    if(run_WhiteMove){
+        if(pawn_Move(start_pos, end_pos, color))
             printf(" valid\n");
         else
             printf(" invalid\n");
-    else
-    if(run_BlackMove)
-        if(pawn_Move(start_pos, end_pos))
+    }else
+    if(run_BlackMove){
+        if(pawn_Move(start_pos, end_pos, color))
             printf(" valid\n");
         else
             printf(" invalid\n");
+    }
+    return 0;
 }
